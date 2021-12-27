5a9e2b193101d3f5a0cb41ad728b8064019f0537
sanchitanwar0007 <sanchitanwar0007@gmail.com>
2021-12-27 12:44:46 +0530

    Assigment resubmitted with necessary changes

---
+++ Assignment-2/code/my-app/src/Data.js
@@ -0,0 +1,8 @@
+const employee = {
+    name: 'Tom',
+    location: 'New York',
+    bloodGroup: 'B+',
+    age: 28,
+  profileImg: 'https://www.w3schools.com/howto/img_avatar.png'
+  }
+  export default employee;
